0788058fbae5021a8ca4a077fc507fe63852be65
Ales Katona <almindor@gmail.com>
2019-06-13 21:46:10 -0600

    refactor + docs

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,9 @@ extern crate embedded_hal;
 
 use embedded_hal::digital::v2::OutputPin;
 
+/// 
+/// Possible command register values on the display chip.
+/// 
 pub enum Command
 {
     Noop = 0x00,
@@ -31,6 +34,9 @@ pub enum Command
     DisplayTest = 0x0F
 }
 
+/// 
+/// Decode modes for BCD encoded input.
+/// 
 pub enum DecodeMode
 {
     NoDecode = 0x00,
@@ -39,7 +45,29 @@ pub enum DecodeMode
     CodeBDigits7_0 = 0xFF
 }
 
-// raised if any of the GPIO pins fails an operation
+/// 
+/// Translate alphanumeric characters into BCD
+/// encoded bytes expected by the display chip.
+/// 
+fn bcd_char(c: char) -> u8 {
+    match c {
+        '-' => 0b00001010, // - without .
+        'e' => 0b00001011, // H without .
+        'E' => 0b10001011, // H with .
+        'h' => 0b00001100, // H without .
+        'H' => 0b10001100, // H with .
+        'l' => 0b00001101, // L without .
+        'L' => 0b10001101, // L with .
+        'p' => 0b00001110, // L without .
+        'P' => 0b10001110, // L with .
+        _ => c as u8,
+    }
+}
+
+///
+/// Error raised in case there was a PIN interaction
+/// error during communication with the MAX7219 chip.
+/// 
 #[derive(Debug)]
 pub struct PinError;
 
@@ -49,6 +77,12 @@ impl From<core::convert::Infallible> for PinError {
     }
 }
 
+///
+/// Handles communication with the MAX7219
+/// chip for segmented displays. Each display can be
+/// connected in series with another and controlled via
+/// a single connection.
+/// 
 pub struct MAX7219<DATA, CS, CLK>
 {
     data: DATA,
@@ -64,7 +98,21 @@ where DATA: OutputPin, CS: OutputPin, CLK: OutputPin,
       PinError: core::convert::From<<CS as embedded_hal::digital::v2::OutputPin>::Error>,
       PinError: core::convert::From<<CLK as embedded_hal::digital::v2::OutputPin>::Error>,
 {
-
+    ///
+    /// Returns a new MAX7219 handler for the displays
+    /// Each display starts blanked, with power and test mode turned off
+    /// 
+    /// # Arguments
+    /// 
+    /// * `devices` - number of displays connected in series
+    /// * `data` - the MOSI/DATA PIN previously set to Output mode
+    /// * `cs` - the CS/SS PIN previously set to Output mode
+    /// * `clk` - the CLK PIN previously set to Output mode
+    ///
+    /// # Errors
+    /// 
+    /// * `PinError` - returned in case there was an error setting a PIN on the device
+    /// 
     pub fn new(devices: u8, data: DATA, cs: CS, clk: CLK) -> Result<Self, PinError> {
 
         let mut num_devices = devices;
@@ -82,9 +130,155 @@ where DATA: OutputPin, CS: OutputPin, CLK: OutputPin,
         Ok(max7219)
     }
 
-    pub fn init(&mut self) -> Result<(), PinError> {
+    ///
+    /// Powers on all connected displays
+    ///
+    /// # Errors
+    /// 
+    /// * `PinError` - returned in case there was an error setting a PIN on the device
+    /// 
+    pub fn power_on(&mut self) -> Result<(), PinError> {
         for i in 0..self.devices {
-            self.write_command(i, Command::DisplayTest)?;
+            self.write_data(i, Command::Power, 0x01)?;
+        }
+
+        Ok(())
+    }
+
+    ///
+    /// Powers off all connected displays
+    ///
+    /// # Errors
+    /// 
+    /// * `PinError` - returned in case there was an error setting a PIN on the device
+    /// 
+    pub fn power_off(&mut self) -> Result<(), PinError> {
+        for i in 0..self.devices {
+            self.write_data(i, Command::Power, 0x00)?;
+        }
+
+        Ok(())
+    }
+
+    ///
+    /// Clears display by settings all digits to empty
+    /// 
+    /// # Arguments
+    /// 
+    /// * `addr` - display to address as connected in series
+    ///
+    /// # Errors
+    /// 
+    /// * `PinError` - returned in case there was an error setting a PIN on the device
+    /// 
+    pub fn clear_display(&mut self, addr: u8) -> Result<(), PinError> {
+        for i in 1..9 {
+            self.write_raw(addr, i, 0x00)?;
+        }
+
+        Ok(())
+    }
+
+    ///
+    /// Sets intensity level on the display
+    /// 
+    /// # Arguments
+    /// 
+    /// * `addr` - display to address as connected in series
+    /// * `intensity` - intensity value to set to `0x00` to 0x0F`
+    ///
+    /// # Errors
+    /// 
+    /// * `PinError` - returned in case there was an error setting a PIN on the device
+    /// 
+    pub fn set_intensity(&mut self, addr: u8, intensity: u8) -> Result<(), PinError> {
+        self.write_data(addr, Command::Intensity, intensity)
+    }
+
+    ///
+    /// Sets decode mode to be used on input sent to the display chip.
+    /// 
+    /// # Arguments
+    /// 
+    /// * `addr` - display to address as connected in series
+    /// * `mode` - the decode mode to set
+    ///
+    /// # Errors
+    /// 
+    /// * `PinError` - returned in case there was an error setting a PIN on the device
+    /// 
+    pub fn set_decode_mode(&mut self, addr: u8, mode: DecodeMode) -> Result<(), PinError> {
+        self.write_data(addr, Command::DecodeMode, mode as u8)
+    }
+
+    ///
+    /// Writes data to given register as described by command
+    /// 
+    /// # Arguments
+    /// 
+    /// * `addr` - display to address as connected in series
+    /// * `command` - the command/register on the display to write to
+    /// * `data` - the data byte value
+    ///
+    /// # Errors
+    /// 
+    /// * `PinError` - returned in case there was an error setting a PIN on the device
+    /// 
+    pub fn write_data(&mut self, addr: u8, command: Command, data: u8) -> Result<(), PinError> {
+        self.write_raw(addr, command as u8, data)
+    }
+
+    ///
+    /// Writes BCD encoded string to the display
+    /// 
+    /// # Arguments
+    /// 
+    /// * `addr` - display to address as connected in series
+    /// * `bcd` - the bcd encoded string slice consisting of [0-9,-,E,L,H,P] where upper case input for alphabetic characters results in dot being set
+    ///
+    /// # Errors
+    /// 
+    /// * `PinError` - returned in case there was an error setting a PIN on the device
+    /// 
+    pub fn write_bcd(&mut self, addr: u8, bcd: &str) -> Result<(), PinError> {
+        self.set_decode_mode(0, DecodeMode::CodeBDigits7_0)?;
+
+        let mut digit: u8 = 8;
+        for c in bcd.chars() {
+            self.write_raw(addr, digit, bcd_char(c))?;
+
+            digit = digit - 1;
+            if digit == 0 {
+                return Ok(())
+            }
+        }
+
+        Ok(())
+    }
+
+    ///
+    /// Set test mode on/off
+    /// 
+    /// # Arguments
+    /// 
+    /// * `addr` - display to address as connected in series
+    /// * `is_on` - whether to turn test mode on or off
+    ///
+    /// # Errors
+    /// 
+    /// * `PinError` - returned in case there was an error setting a PIN on the device
+    /// 
+    pub fn test(&mut self, addr: u8, is_on: bool) -> Result<(), PinError> {
+        if is_on {
+            self.write_data(addr, Command::DisplayTest, 0x01)
+        } else {
+            self.write_data(addr, Command::DisplayTest, 0x00)
+        }
+    }
+
+    fn init(&mut self) -> Result<(), PinError> {
+        for i in 0..self.devices {
+            self.test(i, false)?;
             self.write_data(i, Command::ScanLimit, 0x07)?;
             self.set_decode_mode(i, DecodeMode::NoDecode)?;
             self.clear_display(i)?;
@@ -94,39 +288,11 @@ where DATA: OutputPin, CS: OutputPin, CLK: OutputPin,
         Ok(())
     }
 
-    pub fn set_decode_mode(&mut self, addr: u8, mode: DecodeMode) -> Result<(), PinError> {
-        self.write_data(addr, Command::DecodeMode, mode as u8)
-    }
-
-    pub fn power_on(&mut self) -> Result<(), PinError> {
-        for i in 0..self.devices {
-            self.write_data(i, Command::Power, 0x01)?;
-        }
-
-        Ok(())
-    }
-
-    pub fn power_off(&mut self) -> Result<(), PinError> {
-        for i in 0..self.devices {
-            self.write_data(i, Command::Power, 0x00)?;
-        }
-
-        Ok(())
-    }
-
-    pub fn write_command(&mut self, addr: u8, command: Command) -> Result<(), PinError> {
-        self.write_data(addr, command, 0x00)
-    }
-
-    pub fn write_data(&mut self, addr: u8, command: Command, data: u8) -> Result<(), PinError> {
-        self.write_raw(addr, command as u8, data)
-    }
-
     fn empty_buffer(&mut self) {
         self.buffer = [0; 8];
     }
 
-    pub fn write_raw(&mut self, addr: u8, header: u8, data: u8) -> Result<(), PinError> {
+    fn write_raw(&mut self, addr: u8, header: u8, data: u8) -> Result<(), PinError> {
         let offset = addr * 2;
         let max_bytes = self.devices * 2;
         self.empty_buffer();
@@ -144,10 +310,6 @@ where DATA: OutputPin, CS: OutputPin, CLK: OutputPin,
         Ok(())
     }
 
-    pub fn set_intensity(&mut self, addr: u8, intensity: u8) -> Result<(), PinError> {
-        self.write_data(addr, Command::Intensity, intensity)
-    }
-
     fn shift_out(&mut self, value: u8) -> Result<(), PinError> {
         for i in 0..8 {
             if value & (1 << (7 - i)) > 0 {
@@ -161,21 +323,5 @@ where DATA: OutputPin, CS: OutputPin, CLK: OutputPin,
         }
 
         Ok(())
-    }
-
-    pub fn clear_display(&mut self, addr: u8) -> Result<(), PinError> {
-        for i in 1..9 {
-            self.write_raw(addr, i, 0x00)?;
-        }
-
-        Ok(())
-    }
-
-    pub fn test(&mut self, addr: u8, is_on: bool) -> Result<(), PinError> {
-        if is_on {
-            self.write_raw(addr, 0x0F, 0x01)
-        } else {
-            self.write_raw(addr, 0x01, 0x00)
-        }
     }
 }
